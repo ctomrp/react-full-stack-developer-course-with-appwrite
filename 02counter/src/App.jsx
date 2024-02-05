14527fa5ef2109c380e5971ed5a41547c9868b0e
ctomrp <cristian.rico.palma@gmail.com>
2024-02-05 15:38:52 -0300

    02counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const addValue = ()=>{
+    setCounter(counter + 1)
+    // react no envia de inmediacion la accion indicada, sino que las reune y envía todas en un lote
+    // por eso al poner varios setCounter el valor incrementado sigue siendo 1
+    // si se desea evitar ese comportamiento, se puede usar el callback de setCounter
+    // setCounter((prevCounter)=>prevCounter + 1)
+    // setCounter((prevCounter)=>prevCounter + 1)
+    // setCounter((prevCounter)=>prevCounter + 1)
+    setCounter(counter + 1)
+    setCounter(counter + 1)
+  }
+
+  const removeValue = ()=>{
+    setCounter(counter - 1)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React course</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>{" "}
+      <button onClick={removeValue}>Remove value</button>
+      <p>footer: </p>
     </>
   )
 }
