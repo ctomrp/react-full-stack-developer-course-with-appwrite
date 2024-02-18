057d9ccbe77bf42211103ccbc16234e7335b4ccf
ctomrp <cristian.rico.palma@gmail.com>
2024-02-17 22:15:56 -0300

    10todocontextLocal

--- 10todocontextLocal/src/contexts/ToDoContext.js
+++ 10todocontextLocal/src/contexts/ToDoContext.js
@@ -3,8 +3,8 @@ import { createContext, useContext } from 'react'
 export const ToDoContext = createContext({
     todos: [
         {
-            id: 1,
-            todo: 'todo message',
+            id: null,
+            todo: '',
             completed: false
         }
     ],
