862685659722f84cece58a798e2d36a04a3762c2
ctomrp <cristian.rico.palma@gmail.com>
2024-02-27 12:44:46 -0300

    appwrite services for db and storage

---
+++ 11blogapp/src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf.js";
+
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, login });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.getCurrentUser();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser() :: ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: deleteSessions() :: ", error);
+    }
+  }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
